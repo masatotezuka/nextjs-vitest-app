b436e28ffe0101435ae5449050c83b1a68fc1ec8
masatotezuka <tez.0731.mst@gmail.com>
2024-06-20 21:24:58 +0900

    set up

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "load-svg",
+      enforce: "pre",
+      transform(_, id) {
+        if (id.endsWith(".svg")) {
+          return "export default () => {}"
+        }
+      },
+    },
+  ],
   test: {
+    globals: true,
     environment: "jsdom",
+    coverage: {
+      reporter: ["text", "json", "html"],
+      all: true,
+    },
+    setupFiles: ["./vitest-setup.ts"],
   },
-});
+})
